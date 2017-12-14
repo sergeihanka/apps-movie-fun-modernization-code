21d510df89377702a94d57dcca2fff7eb1acadb0
meerkat3 <sergeiahanka@gmail.com>
2017-12-14 13:52:59 -0500

    added service discovery

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.superbiz.moviefun;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,8 +10,8 @@ import org.springframework.web.client.RestTemplate;
 @Profile("development")
 @Configuration
 public class DevelopmentSecurityConfig {
-
     @Bean
+    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
